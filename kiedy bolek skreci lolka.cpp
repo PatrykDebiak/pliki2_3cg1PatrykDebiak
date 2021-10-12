a7a7295814fd4866e367f8980c8d9f35d2858107
PatrykDebiak <91215263+PatrykDebiak@users.noreply.github.com>
2021-10-12 14:59:08 +0200

    Add files via upload

---
+++ kiedy bolek skreci lolka.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+
+using namespace std;
+
+int main()
+{
+    fstream plik;
+    string x;
+    ofstream myfile;
+    plik.open("hasla.txt", ios::in);
+    myfile.open("passwords.txt", ios::app);
+    if (plik.good() == true)
+    {
+        while (!plik.eof())
+        {
+            plik >> x;
+            int dl = x.length();
+            
+            if (dl >= 8){
+                if (x.find('$') || x.find('#') || x.find('*')) {
+                    myfile<<x<<"\n";
+                }
+                else {
+                    break;
+                }
+            }
+
+
+        }
+        myfile.close();
+        plik.close();
+
+
+    }
+}
